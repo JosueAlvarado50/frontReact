60d609ecebf8c69c060419709f6014270d47d367
josue-cap <Josue.alvarado.cap@gmail.com>
2024-06-30 17:54:56 -0600

    adding command to up the vite service

--- frontMUIReact/src/assets/Components/FoodOrderApp/services/OrderService.js
+++ frontMUIReact/src/assets/Components/FoodOrderApp/services/OrderService.js
@@ -1,36 +1,33 @@
 import axios from "axios";
-const REST_API_BASE_URL = "http://34.121.33.230:8081/api/foodorderapp/meals";
-const USERNAME = "admin";
-const PASSWORD = "admin";
+const REST_API_BASE_URL = "http://34.121.33.230:8081/api/food-order-app/order";
 
 const headers = {
   Authorization: localStorage.getItem("token"),
   "Content-Type": "application/json", // Asegúrate de establecer el tipo de contenido adecuado si es necesario
 };
-
-export const mealList = () => {
+export const OrderList = () => {
   return axios.get(REST_API_BASE_URL, { headers: headers });
 };
 
-export const getMealById = (id) => {
+export const getOrderById = (id) => {
   return axios.get(REST_API_BASE_URL + "/" + id, {
     headers: headers,
   });
 };
 
-export const createMeal = (meal) => {
-  return axios.post(REST_API_BASE_URL, meal, {
+export const createOrder = (order) => {
+  return axios.post(REST_API_BASE_URL, order, {
     headers: headers,
   });
 };
 
-export const updateMeal = (id, meal) => {
-  return axios.put(REST_API_BASE_URL + "/" + id, meal, {
+export const updateOrder = (id, order) => {
+  return axios.put(REST_API_BASE_URL + "/" + id, order, {
     headers: headers,
   });
 };
 
-export const deleteMeal = (id) => {
+export const deleteOrder = (id) => {
   return axios.delete(REST_API_BASE_URL + "/" + id, {
     headers: headers,
   });
